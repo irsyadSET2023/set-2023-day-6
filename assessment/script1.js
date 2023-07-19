32fb12e6c239df9b8ab40a93c5b1c8a5dfb99595
irsyadSET2023 <mirsyad818@gmail.com>
2023-07-19 21:02:56 +0800

    adjust

--- assessment/script1.js
+++ assessment/script1.js
@@ -2,42 +2,33 @@ let videoPlayer = document.querySelector("#videoPlayer");
 
 let videoName = document.querySelector("#videoPlayer source");
 
-// console.log(videoName.src);
+let playBtn = document.querySelector("#play i");
 
-// videoPlayer.setAttribute("src", "./assests2/Opening-013.mp4");
-
-let playBtn = document.querySelector("#play img");
+let videoTitle = document.querySelector("#title h2");
 
 const listVideo = [
   "Opening-001.mp4",
+  "Opening-007.mp4",
   "Opening-011.mp4",
   "Opening-020.mp4",
   "Opening-012.mp4",
   "Opening-013.mp4",
 ];
 
+videoTitle.innerHTML = "Opening - 001";
+
 let videoHead = "http://127.0.0.1:5500/assests2/";
 
-// let currentVideo = videoName.src;
-// console.log(currentVideo.replace(videoHead, ""));
-
-// console.log(listVideo.indexOf("Opening-001"));
-
-// console.log(listVideo.findIndex(currentVideo.replace(videoHead, "")));
-
 listVideo.sort();
-// console.log(listVideo);
 
 function videoPlay() {
   if (videoPlayer.paused) {
-    playBtn.setAttribute("src", "./assests2/images.png");
+    playBtn.className = "fa fa-pause";
 
     videoPlayer.play();
   } else {
-    playBtn.setAttribute(
-      "src",
-      "./assests2/play-button-icon-design-illustration-vector.jpg"
-    );
+    playBtn.className = "fa fa-play";
+
     videoPlayer.pause();
   }
 }
@@ -62,6 +53,10 @@ function nextVideo() {
     videoName.src = `http://127.0.0.1:5500/assests2/${nextPlaylist}`;
     videoPlayer.setAttribute("src", `./assests2/${nextPlaylist}`);
   }
+
+  playBtn.className = "fa fa-play";
+
+  videoTitle.innerHTML = `${nextPlaylist}`.replace(".mp4", "");
 }
 
 function previousVideo() {
@@ -74,7 +69,7 @@ function previousVideo() {
   console.log("Index Video type", typeof indexVideo);
 
   if (indexVideo == 0) {
-    alert("Last Video");
+    alert("First Video");
   } else {
     console.log(indexVideo);
     nextPlaylist = listVideo[indexVideo - 1];
@@ -84,4 +79,47 @@ function previousVideo() {
     videoName.src = `http://127.0.0.1:5500/assests2/${nextPlaylist}`;
     videoPlayer.setAttribute("src", `./assests2/${nextPlaylist}`);
   }
+
+  playBtn.className = "fa fa-play";
+
+  videoTitle.innerHTML = `${nextPlaylist}`.replace(".mp4", "");
+}
+
+function gotoVideo(video) {
+  videoName.src = `http://127.0.0.1:5500/assests2/${video}.mp4`;
+  videoPlayer.setAttribute("src", `./assests2/${video}.mp4`);
+  videoTitle.innerHTML = video;
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
